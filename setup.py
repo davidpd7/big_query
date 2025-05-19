eba071ba65d1354c3479412ee6995d1c3b24d59f
davidpd7 <david.5697.9@gmail.com>
2025-05-19 20:31:13 +0200

    update setup

--- setup.py
+++ setup.py
@@ -2,16 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="big_query",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(where="src"),
     install_requires=[
         "pandas",
         "google-cloud-bigquery",
         "google-auth"
     ],
-    package_data={
-        "src.config": [""],
-    },
+    package_dir={"": "src"},
     python_requires=">=3.7",
     description="Paquete para la integración con Google BigQuery",
     author="David Pérez"
